aa0e2009e6dbfc6ae14290615d3f0fbc51252021
xunandtek <72821027+xunandtek@users.noreply.github.com>
2023-05-19 15:35:14 -0700

    parse json

---
+++ options.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require 'config.php'; 
+
+$file = "C:\Users\yound\OneDrive\Desktop\options.json.txt";
+$json = json_decode(file_get_contents($file), true);
+
+foreach ($json as $select)
+{
+    // print_r($select);
+    foreach ($select as $option)
+    {
+        print_r($option);
+    }
+}
+
+
+?>
